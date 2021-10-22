d50393343aca5f08c7c35224784f152fd78d060a
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-10-22 10:39:09 -0700

    Revert "drm/scheduler: use new iterator in drm_sched_job_add_implicit_dependencies v2"
    This reverts commit 9c2ba265352afc633a1e24d89c3ca499a9e429f4.

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -699,16 +699,30 @@ int drm_sched_job_add_implicit_dependencies(struct drm_sched_job *job,
 					    struct drm_gem_object *obj,
 					    bool write)
 {
-	struct dma_resv_iter cursor;
-	struct dma_fence *fence;
 	int ret;
+	struct dma_fence **fences;
+	unsigned int i, fence_count;
 
-	dma_resv_for_each_fence(&cursor, obj->resv, write, fence) {
-		ret = drm_sched_job_add_dependency(job, fence);
-		if (ret)
-			return ret;
+	if (!write) {
+		struct dma_fence *fence = dma_resv_get_excl_unlocked(obj->resv);
+
+		return drm_sched_job_add_dependency(job, fence);
 	}
-	return 0;
+
+	ret = dma_resv_get_fences(obj->resv, NULL, &fence_count, &fences);
+	if (ret || !fence_count)
+		return ret;
+
+	for (i = 0; i < fence_count; i++) {
+		ret = drm_sched_job_add_dependency(job, fences[i]);
+		if (ret)
+			break;
+	}
+
+	for (; i < fence_count; i++)
+		dma_fence_put(fences[i]);
+	kfree(fences);
+	return ret;
 }
 EXPORT_SYMBOL(drm_sched_job_add_implicit_dependencies);
 
